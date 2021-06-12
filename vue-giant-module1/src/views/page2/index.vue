7a2042dce2817eb751d3f45ccff0494775578f98
凌柏超 <1585638808@qq.com>
2021-06-12 21:21:18 +0800

    修改demo页面

--- vue-giant-module1/src/views/page2/index.vue
+++ vue-giant-module1/src/views/page2/index.vue
@@ -1,8 +1,21 @@
-<template>
-  <div class="page2">
-    <router-link to="basic">basic</router-link>
+<style lang="scss" scoped>
+.page2 {
+  box-sizing: border-box; padding: 15px; height: 100%
+}
+</style>
 
-    <Page2Form1></Page2Form1>
+<template>
+  <div
+    class="page2"
+  >
+    <el-tabs
+      tab-position="left"
+      style="height: 100%; overflow: auto"
+    >
+      <el-tab-pane label="form1">
+        <Page2Form1></Page2Form1>
+      </el-tab-pane>
+    </el-tabs>
 
     <router-view></router-view>
   </div>

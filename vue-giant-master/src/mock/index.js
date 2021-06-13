60fef7d837b7fb6c43e5f0974c38c0b2c850f186
凌柏超 <1585638808@qq.com>
2021-06-13 08:13:53 +0800

    添加一个mock接口

--- vue-giant-master/src/mock/index.js
+++ vue-giant-master/src/mock/index.js
@@ -23,3 +23,56 @@ Mock.mock('/api/basic-table', (req, res) => { // 当post或get请求到/api/data
     tableData: list
   }
 })
+
+Mock.mock('/api/options-1', () => {
+  const options = [
+    {
+      title: '案件编号',
+      roleData: 'caseNo', // 当前表头对应的来自父组件数据源的字段
+      width: '300'
+    },
+    {
+      title: '通知书编号',
+      roleData: 'notificationNumber',
+      width: '300'
+    },
+    {
+      title: '当事人',
+      roleData: 'litigantOrgName'
+    },
+
+    {
+      title: '罚款金额',
+      roleData: 'amountMoney'
+    },
+    {
+      title: '案发日期',
+      roleData: 'occurredTime',
+      width: '230'
+    },
+    {
+      title: '立案日期',
+      roleData: 'registerDate',
+      width: '200'
+    },
+    {
+      title: '案发地点',
+      roleData: 'address'
+    },
+    {
+      title: '执法部门',
+      roleData: 'lawEnforcementDeptName'
+    },
+    {
+      title: '创建人',
+      roleData: 'createUserName'
+    },
+    {
+      title: '状态',
+      roleData: 'caseStatus'
+    }
+  ]
+  return {
+    data: options
+  }
+})

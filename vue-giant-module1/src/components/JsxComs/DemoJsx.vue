539120bfa702eb6a584c35d7e287b9c651cba58a
凌柏超 <1585638808@qq.com>
2021-06-14 06:59:12 +0800

    Create DemoJsx.vue

---
+++ vue-giant-module1/src/components/JsxComs/DemoJsx.vue
@@ -0,0 +1,26 @@
+<style lang="scss">
+$tag: "DemoJsx";
+$sel: "." + $tag;
+
+#{$sel} {
+}
+</style>
+
+<script>
+export default {
+  name: 'DemoJsx',
+  data() {
+    return {}
+  },
+  methods: {
+    handleClick() {
+      console.log('handleClick')
+    }
+  },
+  render(createElement, context) {
+    return (
+      <button onClick={this.handleClick}>button</button>
+    )
+  }
+}
+</script>

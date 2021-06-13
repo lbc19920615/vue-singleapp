f68342706faa8cc6c3b07d0b5d35a21c82939280
凌柏超 <1585638808@qq.com>
2021-06-13 17:25:40 +0800

    添加remote options文档

--- vue-giant-module1/src/views/page3/index.vue
+++ vue-giant-module1/src/views/page3/index.vue
@@ -19,18 +19,29 @@ v-html="doc.remoteSelect[0].content"
           </pre>
         </el-col>
       </el-row>
+      <h3>RemoteOptions Attributes</h3>
+      <el-row>
+        <el-col :span="24">
+          <free-table
+            border
+            stripe
+            :data="doc.tableData"
+            :column="doc.basicColumn"
+          ></free-table>
+        </el-col>
+      </el-row>
     </div>
   </com-page>
 </template>
 
 <script>
-
+import FreeTable from '@/components/FreeTable'
 import RemoteOptions from '@/components/FreeForm/RemoteOptions'
 import {remoteOptionsDemo1Mixin} from '@/views/page3/mixins/common'
 import {docMixin} from '@/views/page3/mixins/doc'
 export default {
   name: 'Page3',
-  components: {RemoteOptions},
+  components: {RemoteOptions, FreeTable},
   mixins: [
     remoteOptionsDemo1Mixin,
     docMixin
--- vue-giant-module1/src/views/page3/mixins/doc.js
+++ vue-giant-module1/src/views/page3/mixins/doc.js
@@ -11,6 +11,28 @@ export const docMixin = {
   data() {
     return {
       doc: {
+        basicColumn: [
+          {label: '参数', prop: 'prop'},
+          {label: '说明', prop: 'desc', minWidth: 150},
+          {label: '类型', prop: 'type'}
+        ],
+        tableData: [
+          {
+            prop: 'label-key',
+            desc: 'el-option label 的 key',
+            type: 'string'
+          },
+          {
+            prop: 'value-key',
+            desc: 'el-option value 的 key',
+            type: 'string'
+          },
+          {
+            prop: 'request-method',
+            desc: '远程搜索方法',
+            type: 'function'
+          }
+        ],
         remoteSelect: [
           registerLanguage('html', `<RemoteOptions
 :request-method="myRequestMethod"
